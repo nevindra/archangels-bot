e8e7e4f225c6aa9e6490db868e16e39048a3cb27
Nevindra <nevindraibnazhifi@gmail.com>
2020-10-27 14:52:58 +0700

    Initial commit

---
+++ commands/other/whomademe.js
@@ -0,0 +1,19 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class WhoMadeMeCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'whomademe',
+      aliases: ['bot-maker', 'bot-creator'],
+      memberName: 'whomademe',
+      group: 'other',
+      description: "Replies with the bot creator's name"
+    });
+  }
+
+  run(message) {
+    message.say(
+      'Made by @Electra#8006 with :heart:'
+    );
+  }
+};
